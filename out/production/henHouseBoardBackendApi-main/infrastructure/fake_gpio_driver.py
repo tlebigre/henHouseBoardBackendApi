24128b7ec8c71d2aaafb29fedf0615d2cb16f495
tlebigre <theo.lebigre@gmail.com>
2026-01-02 22:50:52 +0100

    Add git ignore

--- out/production/henHouseBoardBackendApi-main/infrastructure/fake_gpio_driver.py
+++
@@ -1,9 +0,0 @@
-class FakeGpioDriver:
-    def __init__(self):
-        self.state = {}
-
-    def read(self, gpio: int) -> bool:
-        return self.state.get(gpio, False)
-
-    def write(self, gpio: int, value: bool):
-        self.state[gpio] = value
--- out/production/henHouseBoardBackendApi-main/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %~dp0
-set PYTHONPATH=%CD%;%CD%\generated
-python main.py
-pause
